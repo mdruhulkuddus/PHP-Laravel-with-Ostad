929efc950ad3d98283e250303c4c95111b708747
mdruhulkuddus <ruhul15-14171@diu.edu.bd>
2023-04-18 19:09:44 +0600

    assignmnet-11

---
+++ Assinment/assignment-11/multiplicationTable.js
@@ -0,0 +1,12 @@
+function multiplicationTable(num) {
+    for (let i = 1; i <= 10; i++) {
+      console.log(`${num} x ${i} = ${num * i}`);
+    }
+  }
+  
+  // Example: generate a multiplication table for the number 7 , 9 and 15
+
+  multiplicationTable(7);
+  multiplicationTable(9);
+  multiplicationTable(15);
+  
---
+++ Assinment/assignment-11/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+
+function multiplicationTable(num) {
+    // Iterate from 1 to 10
