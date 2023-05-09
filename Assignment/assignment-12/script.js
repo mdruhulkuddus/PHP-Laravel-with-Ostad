f763438d60ede59859af2988c7b0df25548fda1b
mdruhulkuddus <ruhul15-14171@diu.edu.bd>
2023-05-09 12:09:39 +0600

    assignment-12 update

--- Assignment/assignment-12/script.js
+++ Assignment/assignment-12/script.js
@@ -1,18 +1,13 @@
-// Get the input fields and button
 let weightInput = document.getElementById("weightInput");
 let heightInput = document.getElementById("heightInput");
 let calculateBtn = document.getElementById("calculateBtn");
 
-// Add click event listener to the button
 calculateBtn.addEventListener("click", function() {
-	// Get the weight and height values from the input fields
 	let weight = parseFloat(weightInput.value);
 	let height = parseFloat(heightInput.value);
 
-	// Calculate the BMI
 	let bmi = weight / (height * height);
 
-	// Display the BMI result on the web page
 	let resultDiv = document.getElementById("result");
 	resultDiv.innerHTML = "Your BMI is: " + bmi.toFixed(2);
 });
