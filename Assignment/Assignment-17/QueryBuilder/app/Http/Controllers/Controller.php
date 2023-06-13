31e73a7575d2d5fe49573a0cca0b3d2b819e769f
mdruhulkuddus <ruhul15-14171@diu.edu.bd>
2023-06-13 08:22:57 +0600

    module - 17

--- Assignment/Assignment-17/QueryBuilder/app/Http/Controllers/Controller.php
+++ Assignment/Assignment-17/QueryBuilder/app/Http/Controllers/Controller.php
@@ -5,17 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use DB;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-
-    function delete() {
-        $affectedRows = DB::table( 'posts' )->where( 'id', 3 )->delete();
-    }
-
-    
 }
-
-
-           
